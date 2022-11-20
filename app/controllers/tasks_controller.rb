dc32c2fce002f6570e41a15233962b9bc801c61a
naoki.sugai <musen1485@gmail.com>
2022-11-20 13:03:31 +0000

    fixed kadailist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,10 +31,10 @@ class TasksController < ApplicationController
       @task = Task.find(params[:id])
       
      if @task.update(task_params)
-         flash[:success] = "Task は正常に更新されました"
+         flash[:success] = 'Task は正常に更新されました'
          redirect_to @task
      else
-         flash.now[:danger] = "Task は更新されませんでした"
+         flash.now[:danger] = 'Task は更新されませんでした'
          render :edit
      end
   end
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
       @task = Task.find(params[:id])
       @task.destroy
       
-      flash[:success] = "Task は正常に削除されました"
+      flash[:success] = 'Task は正常に削除されました'
       redirect_to tasks_url
   end
   
