414a88bddb6a8eada7e5ea55c749214dc58f5e1e
naoki.sugai <musen1485@gmail.com>
2022-11-30 15:23:01 +0000

    yes

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_132212) do
-
-  create_table "maketasks", charset: "utf8mb4", force: :cascade do |t|
-    t.string "content"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_maketasks_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2022_11_30_150654) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "task"
     t.string "status"
   end
 
@@ -36,5 +27,4 @@ ActiveRecord::Schema.define(version: 2022_11_30_132212) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "maketasks", "users"
 end
