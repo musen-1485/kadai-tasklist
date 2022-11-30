a3589450fc0aead77f7c585065707f02078e2850
naoki.sugai <musen1485@gmail.com>
2022-11-30 13:17:46 +0000

    login sistem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_action :require_user_logged_in, only: [:index, :show]
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
@@ -11,7 +12,6 @@ class UsersController < ApplicationController
      @user = User.new
   end
 
-  def create
   def create
     @user = User.new(user_params)
 
@@ -29,5 +29,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
+   
     root to: 'tasks#index'
     
     get 'login', to: 'sessions#new'
