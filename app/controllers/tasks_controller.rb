4a698b0e0e436c08317b941df25345663fe5b6df
naoki.sugai <musen1485@gmail.com>
2022-12-06 12:22:41 +0000

    deploy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    # before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :correct_user, only: [:show, :update, :destroy]
     
     def index
@@ -46,9 +46,9 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-      @task = Task.find(params[:id])
-  end
+#   def set_task
+#       @task = Task.find(params[:id])
+#   end
   #Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status, :user_id)
